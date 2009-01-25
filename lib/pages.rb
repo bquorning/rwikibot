9bc3bcde173a19b3efac8efa7e597fbadb5e2ae0
Benjamin Quorning <benjamin@quorning.net>
2009-01-25 20:59:10 +0100

    Simplify Page.exists?

--- lib/pages.rb
+++ lib/pages.rb
@@ -26,11 +26,7 @@ module Pages
     
     # I used to have an exists method (page_exists), but I got rid of it in 2.0, but I'm bringing it back. 
     def exists?
-      if @missing
-        return false
-      else
-        return true
-      end
+      !@missing
     end
 
     # This will get only the content of the article. It is a modification of
